64b634d62c0d08cc779eb2f6133597e76987402b
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2018-08-12 17:10:14 +0100

    meaningful failing test

--- src/test/kotlin/rbtree/tests.kt
+++ src/test/kotlin/rbtree/tests.kt
@@ -5,10 +5,28 @@ import org.junit.Test
 
 class TestSource {
     @Test fun NodeRightRotate() {
-        val n1 = Node<Long>(3)
-        val n2 = Node<Long>(10)
-        n1.addRightNode(n2)
-        assertEquals(n2, n1.right)
-        assertEquals(1, 2)
+        val parent = Node<Long>(0)
+        val y = Node<Long>(3)
+        parent.addLeftNode(y)
+        val x = Node<Long>(10)
+        val alpha = Node<Long>(20)
+        val beta = Node<Long>(30)
+        val gamma = Node<Long>(15)
+        x.addLeftNode(alpha)
+        x.addRightNode(beta)
+        y.addLeftNode(x)
+        y.addRightNode(gamma)
+        y.rightRotate()
+
+        assertEquals(parent.left, x)
+        assertEquals(x.parent, parent)
+        assertEquals(x.left, alpha)
+        assertEquals(alpha.parent, x)
+        assertEquals(x.right, y)
+        assertEquals(y.parent, x)
+        assertEquals(y.left, beta)
+        assertEquals(beta.parent, y)
+        assertEquals(y.right, gamma)
+        assertEquals(gamma.parent, y)
     }
 }
